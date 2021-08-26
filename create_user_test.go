3b1e1b9bbec32a071dbd75a0e2c7443902aaad6a
LSS-Dipali-Sable <72379579+Dipali-Sable@users.noreply.github.com>
2021-08-26 18:44:32 +0530

    Added the validation of the responses

--- create_user_test.go
+++ create_user_test.go
@@ -21,20 +21,32 @@ func TestCreateUser(t *testing.T) {
 }
 
 func call_create_user_api(t gobdd.TestingT, ctx context.Context) context.Context {
-	jsonData := map[string]string{"name": "morpheus", "job": "leader"}
+	jsonData := map[string]string{"name": "Morpheus", "job": "Leader"}
 	jsonValue, _ := json.Marshal(jsonData)
 
 	response, err := http.Post("https://reqres.in/api/users", "application/json", bytes.NewBuffer(jsonValue))
 
 	if err != nil {
 		fmt.Printf("The HTTP request failed with error %s\n", err)
-	} else {
-		data, _ := ioutil.ReadAll(response.Body)
-		fmt.Println(string(data))
 	}
 
 	assert.Equal(t, "201 Created", response.Status)
 	assert.Equal(t, 201, response.StatusCode)
 	assert.NotNil(t, response.Body)
+
+	//check the values of the keys
+	data, _ := ioutil.ReadAll(response.Body)
+	fmt.Println(string(data))
+
+	var responseObject ResponseData
+	json.Unmarshal(data, &responseObject)
+	assert.Equal(t, responseObject.Name, "Morpheus")
+	assert.Equal(t, responseObject.Job, "Leader")
+
 	return ctx
+}
+
+type ResponseData struct {
+	Name string `json:"name"`
+	Job  string `json:"job"`
 }
--- userinfo_test.go
+++ userinfo_test.go
@@ -1,6 +1,7 @@
 package main_test
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -34,5 +35,37 @@ func callapi(t gobdd.TestingT, ctx context.Context) context.Context {
 	assert.Equal(t, http.StatusOK, response.StatusCode)
 	assert.Equal(t, 200, response.StatusCode)
 	assert.NotNil(t, response.Body)
+
+	//Validate the response Body
+	var responseObject Response
+	json.Unmarshal(responseData, &responseObject)
+	assert.Equal(t, responseObject.Page, 1)
+	assert.Equal(t, responseObject.Per_page, 6)
+	assert.Equal(t, responseObject.Total, 12)
+	assert.Equal(t, responseObject.Total_pages, 2)
+
+	//read the user data
+	for i := 0; i < len(responseObject.User); i++ {
+		fmt.Println(responseObject.User[i].Id)
+		fmt.Println(responseObject.User[i].email)
+		fmt.Println(responseObject.User[i].First_Name)
+		fmt.Println(responseObject.User[i].Last_Name)
+	}
 	return ctx
+}
+
+type Response struct {
+	Page        int    `json:"page"`
+	Per_page    int    `json:"per_page"`
+	Total       int    `json:"total"`
+	Total_pages int    `json:"total_pages"`
+	User        []User `json:"data"`
+}
+
+type User struct {
+	Id         int    `json:"id"`
+	email      string `json:"email"`
+	First_Name string `json:"first_name"`
+	Last_Name  string `json:"last_name"`
+	Avatar     string `json:"avatar"`
 }
